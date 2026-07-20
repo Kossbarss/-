8266bb52b44e39116a916941455aff36a7941195
Kossbarss <kostik.barss@gmail.com>
2026-07-20 21:15:28 +0300

    Scope hero front-facing transform fix to gallery cards

--- hero-front-facing.js
+++ hero-front-facing.js
@@ -9,54 +9,36 @@
 
   shell.dataset.frontFacingCards = 'true'
 
-  const targetStyles = new WeakSet(cards.map((card) => card.style))
-  const normalizeTransform = (value) => String(value || '').replace(/rotateY\([^)]*\)/g, 'rotateY(0deg)')
-
-  let owner = CSSStyleDeclaration.prototype
-  let descriptor = null
-
-  while (owner && !descriptor) {
-    descriptor = Object.getOwnPropertyDescriptor(owner, 'transform')
-    if (!descriptor) owner = Object.getPrototypeOf(owner)
+  function normalizeTransform(value) {
+    return String(value || '').replace(/rotateY\([^)]*\)/g, 'rotateY(0deg)')
   }
 
-  if (owner && descriptor && descriptor.get && descriptor.set && descriptor.configurable) {
-    Object.defineProperty(owner, 'transform', {
-      configurable: descriptor.configurable,
-      enumerable: descriptor.enumerable,
-      get: descriptor.get,
-      set(value) {
-        return descriptor.set.call(this, targetStyles.has(this) ? normalizeTransform(value) : value)
-      },
+  let scheduled = false
+
+  function normalizeCards() {
+    scheduled = false
+    cards.forEach((card) => {
+      const current = card.style.transform
+      const next = normalizeTransform(current)
+      if (next !== current) card.style.transform = next
     })
   }
 
-  const nativeSetProperty = CSSStyleDeclaration.prototype.setProperty
-  CSSStyleDeclaration.prototype.setProperty = function (property, value, priority) {
-    const nextValue = targetStyles.has(this) && property === 'transform'
-      ? normalizeTransform(value)
-      : value
-    return nativeSetProperty.call(this, property, nextValue, priority)
+  function scheduleNormalize() {
+    if (scheduled) return
+    scheduled = true
+    requestAnimationFrame(normalizeCards)
   }
 
-  function normalizeCard(card) {
-    const current = card.style.transform
-    const next = normalizeTransform(current)
-    if (next !== current) card.style.transform = next
+  normalizeCards()
+
+  if ('MutationObserver' in window) {
+    const observer = new MutationObserver(scheduleNormalize)
+    cards.forEach((card) => observer.observe(card, {
+      attributes: true,
+      attributeFilter: ['style'],
+    }))
   }
 
-  cards.forEach(normalizeCard)
-
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-        normalizeCard(mutation.target)
-      }
-    })
-  })
-
-  cards.forEach((card) => observer.observe(card, {
-    attributes: true,
-    attributeFilter: ['style'],
-  }))
+  window.addEventListener('resize', scheduleNormalize, { passive: true })
 })()
