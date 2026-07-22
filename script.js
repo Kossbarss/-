aa74d7a77baef1e5ca3d9e3c37de6e6d36e07a42
Kossbarss <kostik.barss@gmail.com>
2026-07-22 18:20:57 +0300

    Stop removing mounted cases carousel

--- script.js
+++ script.js
@@ -4,19 +4,7 @@
   window.VIP_TATTOO_LOCALE = 'ru'
   const modules = ['site-core.js', 'site-interactions.js']
 
-  function clearCasesComponents() {
-    const wrap = document.querySelector('.case-fan-wrap')
-    if (wrap) {
-      const mount = document.createElement('div')
-      mount.id = 'caseComponentMount'
-      mount.className = 'case-component-mount'
-      wrap.replaceChildren(mount)
-    }
-    document.querySelector('.case-fan-cta')?.remove()
-  }
-
   function finish() {
-    clearCasesComponents()
     window.__vipTattooAppReady = true
     document.dispatchEvent(new CustomEvent('vip:app-ready'))
   }
